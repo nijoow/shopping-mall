b941fb0d9d08579f31eaaec0e0b1544c889665e5
Woojin Lee <nijoow1127@gmail.com>
2023-12-03 00:54:33 +0900

    🎨 prettier 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Nijoow Shopping Mall",
-  description: "Shopping Mall Toy Project",
+  title: 'Nijoow Shopping Mall',
+  description: 'Shopping Mall Toy Project',
 };
 
 export default function RootLayout({
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko" className="h-full">
+      <body className={`${inter.className} h-full`}>{children}</body>
     </html>
   );
 }
