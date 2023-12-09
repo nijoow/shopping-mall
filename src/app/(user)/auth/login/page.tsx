199744140f7ec1f332c3e56d05c4378c94d8c15d
Woojin Lee <nijoow1127@gmail.com>
2023-12-10 00:00:55 +0900

    💄 로그인 페이지 UI 생성

---
+++ src/app/(user)/auth/login/page.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="flex flex-col w-full max-w-sm gap-6">
+        <span className="mx-auto font-semibold text-2 text-rose-400">
+          로그인
+        </span>
+        <div className="w-full h-1 bg-rose-400" />
+        <div className="flex flex-col gap-2">
+          <input
+            type="text"
+            placeholder="아이디"
+            className="p-3 border border-gray-300"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            className="p-3 border border-gray-300"
+          />
+        </div>
+        <button className="p-3 text-white bg-rose-400">로그인</button>
+        <div className="flex items-center justify-center w-full gap-4 text-gray-400">
+          <Link href={'/auth/login/find/id'}>아이디 찾기</Link>
+          <div className="w-0.5 h-3.5 bg-gray-300" />
+          <Link href={'/auth/login/find/password'}>비밀번호 찾기</Link>
+        </div>
+        <button className="p-3 border text-rose-400 border-rose-400">
+          회원가입
+        </button>
+      </div>
+    </div>
+  );
+}
