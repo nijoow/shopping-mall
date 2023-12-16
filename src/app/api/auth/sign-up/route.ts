2457a3262677b5c510db5e6c5e55de642f59f41f
Woojin Lee <nijoow1127@gmail.com>
2023-12-16 22:23:37 +0900

    🎨 password 암호화

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,15 +1,22 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import * as bcrypt from 'bcrypt';
 
 export async function POST(request: Request) {
   const body = await request.json();
 
   const { userId, password, name } = body;
+  const duplicated = await sql`SELECT * FROM users WHERE userId = ${userId};`;
+
+  if (duplicated)
+    return NextResponse.json({ error: 'duplicated' }, { status: 400 });
 
   try {
     if (!userId || !password) throw new Error('userId and password required');
-    await sql`INSERT INTO users (userId, password, name) VALUES (${userId}, ${password}, ${name});`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await sql`INSERT INTO users (userId, password, name) VALUES (${userId}, ${hashedPassword}, ${name});`;
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 
