d4de32305697eb2238fc9a8645e2398bb413ced3
Woojin Lee <nijoow1127@gmail.com>
2023-12-17 02:23:06 +0900

    🎨 로그인 기능 추가

--- src/app/(user)/auth/login/page.tsx
+++ src/app/(user)/auth/login/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
 import Link from 'next/link';
 import SocialLogin from './_compontents/SocialLogin';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { signIn } from 'next-auth/react';
+
+type LoginInput = {
+  userId: string;
+  password: string;
+};
 
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<LoginInput>();
+
+  const onSubmit: SubmitHandler<LoginInput> = submitData => {
+    signIn('credentials', submitData);
+  };
+
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="flex flex-col w-full max-w-sm gap-6">
@@ -9,19 +29,21 @@ export default function LoginPage() {
           로그인
         </span>
         <div className="w-full h-1 bg-rose-400" />
-        <div className="flex flex-col gap-2">
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+          <label>아이디</label>
           <input
-            type="text"
-            placeholder="아이디"
+            {...register('userId', { required: true })}
             className="p-3 border border-gray-300"
           />
+          <label>패스워드</label>
           <input
-            type="password"
-            placeholder="비밀번호"
+            {...register('password', { required: true })}
             className="p-3 border border-gray-300"
           />
-        </div>
-        <button className="p-3 text-white bg-rose-400">로그인</button>
+          <button type="submit" className="p-3 text-white bg-rose-400">
+            로그인
+          </button>
+        </form>
         <div className="flex items-center justify-center w-full gap-4 text-gray-400">
           <Link href={'/auth/login/find/id'}>아이디 찾기</Link>
           <div className="w-0.5 h-3.5 bg-gray-300" />
