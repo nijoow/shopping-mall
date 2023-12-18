710a2be5e669cbb8a5b8a18c77861d596ce56e4a
Woojin Lee <nijoow1127@gmail.com>
2023-12-19 00:47:11 +0900

    🎨 배너이미지 추가

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col w-full h-full ">
+      <div className="w-full h-[800px] relative">
+        <Image
+          src={'/images/banner/main.jpg'}
+          alt="배너이미지"
+          fill
+          className="object-cover object-[0_58%]"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
