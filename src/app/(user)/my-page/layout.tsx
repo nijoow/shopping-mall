5a41e4f665fd52399a5f607995920d0fa5fd8c94
Woojin Lee <nijoow1127@gmail.com>
2024-01-11 01:44:08 +0900

    🎨 my-page 레이아웃 분리

---
+++ src/app/(user)/my-page/layout.tsx
@@ -0,0 +1,37 @@
+import { auth } from 'auth';
+import Link from 'next/link';
+import React from 'react';
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
+  return (
+    <div className="p-8 flex h-full w-full">
+      <div className="gap-8 w-64 h-full flex flex-col">
+        <span className="text-2.25 font-semibold">
+          {session?.user.nickname}
+        </span>
+        <span>좋아요 0</span>
+        <div className="flex flex-col gap-4">
+          <span className="text-1.25 font-semibold">쇼핑 정보</span>
+          <span className="text-gray-500 font-light">주문배송조회</span>
+          <span className="text-gray-500 font-light">취소/교환/반품 내역</span>
+          <span className="text-gray-500 font-light">상품 리뷰</span>
+        </div>
+        <div className="flex flex-col gap-4">
+          <span className="text-1.25 font-semibold">계정 설정</span>
+          <Link
+            href={'/my-page/information'}
+            className="text-gray-500 font-light"
+          >
+            회원정보
+          </Link>
+          <span className="text-gray-500 font-light">포인트</span>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-50 p-8 flex"> {children}</div>
+    </div>
+  );
+};
+
+export default Layout;
--- src/app/(user)/my-page/page.tsx
+++ src/app/(user)/my-page/page.tsx
@@ -1,22 +1,5 @@
-import { auth } from 'auth';
 import React from 'react';
 
 export default async function MyPage() {
-  const session = await auth();
-
-  return (
-    <div className="p-8 flex h-full w-full">
-      <div className="gap-8 w-64 h-full flex flex-col">
-        <span className="text-2.25 font-semibold">{session?.user?.name}</span>
-        <span>좋아요 0</span>
-        <div className="flex flex-col gap-4">
-          <span className="text-1.25 font-semibold">나의 쇼핑정보</span>
-          <span className="text-gray-500 font-light">주문배송조회</span>
-          <span className="text-gray-500 font-light">취소/교환/반품 내역</span>
-          <span className="text-gray-500 font-light">상품 리뷰</span>
-        </div>
-      </div>
-      <div className="w-full h-full bg-gray-50"> </div>
-    </div>
-  );
+  return <div>마이페이지 메인 </div>;
 }
