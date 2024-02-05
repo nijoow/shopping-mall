12bd5e6cb6a5029b329cb3bff0cfbd81e80e984d
Woojin Lee <nijoow1127@gmail.com>
2024-02-05 23:21:09 +0900

    🎨 배송지 추가 Form 수정

--- src/app/(user)/my-page/addresses/_compontent.tsx/AddAddress.tsx
+++ src/app/(user)/my-page/addresses/_compontent.tsx/AddAddress.tsx
@@ -3,32 +3,45 @@ import Button from '@/components/Button';
 import Modal from '@/components/Modal';
 import React, { useState } from 'react';
 import { IoAdd } from 'react-icons/io5';
-import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { phoneRegex } from '@/lib/utils/regex';
 import { useDaumPostcodePopup } from 'react-daum-postcode';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { formatPhoneNumber } from '@/lib/utils/formatPhoneNumber';
 
 const addressFormSchema = z.object({
-  name: z.string().nullable(),
-  gender: z.enum(['MALE', 'FEMALE']).nullable(),
-  birth: z
-    .object({ year: z.number(), month: z.number(), day: z.number() })
-    .nullable(),
-  phone_number: z.string().regex(phoneRegex).nullable(),
+  name: z.string(),
+  phoneNumber: z.string().regex(phoneRegex),
+  postCode: z.string(),
+  address: z.string(),
+  detailAddress: z.string().nullable(),
 });
+
+type AddressFormInput = z.infer<typeof addressFormSchema>;
 
 const AddAddress = () => {
   const open = useDaumPostcodePopup();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [address, setAddress] = useState<Record<string, string | null>>({
-    postCode: null,
-    address: null,
-    detailAddress: null,
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<AddressFormInput>({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
   });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+
+  const onChangePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 13) return;
+    setValue('phoneNumber', formatPhoneNumber(event.target.value));
+  };
 
   const handleComplete = (data: any) => {
     let fullAddress = data.address;
@@ -45,12 +58,14 @@ const AddAddress = () => {
       fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
     }
 
-    setAddress({
-      postCode: data.zonecode,
-      address: fullAddress,
-      detailAddress: '',
-    });
+    setValue('postCode', data.zonecode);
+    setValue('address', fullAddress);
   };
+
+  const onSubmit: SubmitHandler<AddressFormInput> = submitData => {
+    console.log(submitData);
+  };
+
   return (
     <>
       <div
@@ -66,14 +81,62 @@ const AddAddress = () => {
         <Modal>
           <Modal.Title closeModal={closeModal}>배송지 추가</Modal.Title>
           <Modal.Body>
-            <form className="flex flex-col gap-4 w-full my-4 ">
+            <form
+              id="address-form"
+              className="flex flex-col gap-4 w-full my-4"
+              onSubmit={handleSubmit(onSubmit)}
+            >
               <label className="flex flex-col gap-1 w-full">
+                <div className="w-full flex flex-col gap-0.5">
+                  <span>이름</span>
+                  <input
+                    placeholder="이름을 입력해주세요"
+                    {...register('name', {
+                      required: '이름은 필수입니다!',
+                    })}
+                    className="px-3 py-2.5 rounded-md text-1 border border-gray-300 flex-auto"
+                  />
+                  {errors.name && (
+                    <span className="text-0.875 text-red-400">
+                      {errors.name.message}
+                    </span>
+                  )}
+                </div>
+              </label>
+              <label className="flex flex-col gap-1 w-full">
+                <div className="w-full flex flex-col gap-0.5">
+                  <span>휴대폰 번호</span>
+                  <input
+                    placeholder="휴대폰 번호를 입력해주세요"
+                    {...register('phoneNumber', {
+                      required: '휴대폰 번호는 필수입니다!',
+                      pattern: {
+                        value: phoneRegex,
+                        message: '휴대폰 번호를 올바르게 입력해주세요!',
+                      },
+                    })}
+                    onChange={onChangePhoneNumber}
+                    value={watch('phoneNumber')}
+                    className="px-3 py-2.5 rounded-md text-1 border border-gray-300 flex-auto"
+                  />
+                  {errors.phoneNumber && (
+                    <span className="text-0.875 text-red-400">
+                      {errors.phoneNumber.message}
+                    </span>
+                  )}
+                </div>
+              </label>
+              <label className="flex flex-col gap-1 w-full">
+                <span>주소</span>
                 <div className="w-full flex items-center gap-1">
                   <input
                     type="text"
                     placeholder="우편번호"
-                    value={address.postCode ?? ''}
                     disabled
+                    {...register('postCode', {
+                      required: '주소찾기 버튼을 통해 주소를 입력해주세요!',
+                    })}
+                    value={watch('postCode')}
                     className="px-3 py-2.5 rounded-md text-1 border border-gray-300 flex-auto"
                   />
                   <Button
@@ -92,19 +155,25 @@ const AddAddress = () => {
                 <input
                   type="text"
                   placeholder="기본 주소"
-                  value={address.address ?? ''}
                   disabled
+                  {...register('address', {
+                    required: '주소찾기 버튼을 통해 주소를 입력해주세요!',
+                  })}
+                  value={watch('address')}
                   className="px-3 py-2.5 rounded-md text-1 border border-gray-300 flex-auto"
                 />
                 <input
                   type="text"
                   placeholder="상세 주소"
-                  value={address.detailAddress ?? ''}
-                  onChange={e =>
-                    setAddress({ ...address, detailAddress: e.target.value })
-                  }
+                  {...register('detailAddress')}
+                  onChange={e => setValue('detailAddress', e.target.value)}
                   className="px-3 py-2.5 rounded-md text-1 border border-gray-300 flex-auto"
                 />
+                {errors.address && (
+                  <span className="text-0.875 text-red-400">
+                    {errors.address.message}
+                  </span>
+                )}
               </label>
             </form>
           </Modal.Body>
@@ -113,7 +182,9 @@ const AddAddress = () => {
               <Button variant="outlined" onClick={closeModal}>
                 취소
               </Button>
-              <Button>저장</Button>
+              <Button type="submit" form="address-form">
+                저장
+              </Button>
             </div>
           </Modal.Footer>
         </Modal>
