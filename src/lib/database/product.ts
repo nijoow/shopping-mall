49ec7b821cd6d8f8064114fb92220a3e0a57a093
Woojin Lee <nijoow1127@gmail.com>
2024-02-28 00:18:53 +0900

    🎨 product DB 조회 코드 추가

--- src/lib/database/product.ts
+++ src/lib/database/product.ts
@@ -1,6 +1,25 @@
 import { Product } from '@/types/types';
 import { sql } from '@vercel/postgres';
 
+export const getProductByProductId = async (
+  productId: number,
+): Promise<Product | undefined> => {
+  try {
+    const user = await sql<Product>`
+        SELECT 
+            * 
+        FROM 
+            products 
+        WHERE 
+            "productId" = ${productId}
+    `;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch product:', error);
+    throw new Error('Failed to fetch product.');
+  }
+};
+
 export const getRecentProducts = async (): Promise<Product[] | undefined> => {
   try {
     const user = await sql<Product>`
