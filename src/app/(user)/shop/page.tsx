31506e7e222f31122c65ef72d140af12a679c9d3
Woojin Lee <nijoow1127@gmail.com>
2024-03-19 00:34:02 +0900

    💄 UI 수정

--- src/app/(user)/shop/page.tsx
+++ src/app/(user)/shop/page.tsx
@@ -1,15 +1,24 @@
 import ProductCard from '@/components/ProductCard';
+import Link from 'next/link';
 import React from 'react';
 
 export default function ShopPage() {
   return (
-    <div className="flex h-full w-full p-8">
-      <div className="flex h-full w-64 flex-col gap-8">
-        <span className="text-1.5 font-semibold">SHOP</span>
-        <div className="flex flex-col gap-2">
-          <span className="font-light text-gray-500">ALL</span>
-          <span className="font-light text-gray-500">NEW</span>
-          <span className="font-light text-gray-500">OUTER</span>
+    <div className="flex h-full w-full flex-col py-4 sm:flex-row sm:p-8">
+      <div className="flex h-full w-full flex-col gap-2 sm:w-64 sm:gap-8">
+        <span className="pl-4 text-1.5 font-semibold sm:pl-0">SHOP</span>
+        <div className="flex w-full flex-row gap-2 overflow-x-auto sm:flex-col">
+          {['ALL', 'NEW', 'OUTER', 'TOP', 'BOTTOM', 'SHOES', 'ACC'].map(
+            category => (
+              <Link
+                href="#"
+                key={category}
+                className="px-2 py-1.5 font-light text-gray-500"
+              >
+                {category}
+              </Link>
+            ),
+          )}
         </div>
         <div className="flex flex-col gap-2">
           <span className="font-semibold">필터</span>
