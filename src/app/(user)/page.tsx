f86153935488a9d2ef06be49ff7fbdb7da3ebb96
Woojin Lee <nijoow1127@gmail.com>
2024-06-10 01:23:35 +0900

    🎨 배너 변경

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,6 +1,5 @@
 import { getRecentProducts } from '@/lib/database/product';
 import { unstable_noStore } from 'next/cache';
-import Image from 'next/image';
 import HomeCarousel from './_components/Product/HomeCarousel';
 
 export default async function HomePage() {
@@ -10,12 +9,25 @@ export default async function HomePage() {
   return (
     <div className="flex w-full flex-auto flex-col gap-10">
       <div className="relative aspect-[16/9] w-full">
-        <Image
+        {/* <Image
           src="/images/banner/banner.webp"
           alt="배너이미지"
           fill
           className="object-cover"
-        />
+        /> */}
+
+        <video className="w-full" autoPlay loop muted>
+          <source
+            src="https://d340a4zb19l6y1.cloudfront.net/24ss/editorial/bang-olufsen/editorial_01.mp4"
+            type="video/mp4"
+          />
+          <track
+            src="captions_en.vtt"
+            kind="captions"
+            srcLang="ko"
+            label="caption"
+          />
+        </video>
       </div>
       {carouselProducts && (
         <HomeCarousel
