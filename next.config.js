a92723ca7029250fd6147914b07844ef2ffa7ecf
Woojin Lee <nijoow1127@gmail.com>
2024-06-10 01:22:57 +0900

    🔧 images remotePatterns 추가

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'adererror.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
