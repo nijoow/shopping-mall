35722958df94492066b20dcc4a0a664570078a2c
Woojin Lee <nijoow1127@gmail.com>
2024-09-19 00:09:09 +0900

    💄 나눔스퀘어라운드 폰트 추가 #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
+import { nanumSquareRound } from '@/fonts/font';
 import ReactQueryProvider from '@/lib/react-query/ReactQueryProvider';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Nijoow Shopping Mall',
@@ -16,7 +14,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <ReactQueryProvider>
       <html lang="ko" className="h-full !scroll-smooth">
-        <body className={`${inter.className} relative flex h-full flex-col`}>
+        <body
+          className={`${nanumSquareRound.className}  relative flex h-full flex-col`}
+        >
           {children}
         </body>
       </html>
